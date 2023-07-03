09fb4ab38bb0f1db6c8bc182b3c35491b24c2639
Mario Kerndler <mariokerndler@gmail.com>
2023-07-03 15:03:00 +0200

    chore(): switched frontend to nuxt

--- 5chan-frontend/tailwind.config.js
+++ 5chan-frontend/tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
